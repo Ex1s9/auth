e608bd6a592c61185d73f3dc9bab64dd61db12e9
Ex1 <sadovskyroma@gmail.com>
2025-09-25 20:39:23 +0300

    style

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,11 @@
-import styles from './page.module.scss';
+import styles from "./page.module.scss";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>Auth System</h1>
-        <p className={styles.description}>
-          Simple authentication system
-        </p>
+        <p className={styles.description}>Simple authentication system</p>
       </main>
     </div>
   );
